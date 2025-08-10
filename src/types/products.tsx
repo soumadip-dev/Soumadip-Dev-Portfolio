09fec8821df4a858d8950c3c7113fb13ed6c9691
soumadip-dev <soumadipmajila@gmail.com>
2025-08-11 01:12:17 +0530

    Allow thumbnail to be either StaticImageData OR string

--- src/types/products.tsx
+++ src/types/products.tsx
@@ -1,9 +1,11 @@
-import { StaticImageData } from "next/image";
+import { StaticImageData } from 'next/image';
 
 export type Product = {
   title: string;
   description: string;
-  thumbnail: StaticImageData;
+  // Allow thumbnail to be either StaticImageData OR string
+  thumbnail: StaticImageData | string;
+  // images can already be StaticImageData[] or string[], so leave it as is
   images: StaticImageData[] | string[];
   href: string;
   slug?: string;
